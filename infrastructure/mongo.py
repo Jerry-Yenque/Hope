23196943df9584a57d8e8f25661fe5354fbf3d49
Jerry Yenque <jerry.yenque@outlook.com>
2025-01-11 14:50:36 -0500

    reload 3.0

--- infrastructure/mongo.py
+++ infrastructure/mongo.py
@@ -5,43 +5,48 @@ import importlib
 from presentation.theme import VERDE, GRIS, AMARILLO, ROJO, AZUL, BLANCO
 import os
 load_dotenv()
-A = 3
 
 class MongoConnection:
-    def __init__(self, uri):
-        self.client = MongoClient(uri)
+    def __init__(self) -> None:
+        load_dotenv()
+        self.host = os.getenv("MONGO_HOST")  # Valor por defecto si no se encuentra
+        # self.db_name = os.getenv("")       # Valor por defecto si no se encuentra
+        # self.client = None
+        self.db = None
+        for name, method in mongo.MongoConnection.__dict__.items():
+            if callable(method):
+                setattr(self, name, method.__get__(self, self.__class__))
+        # pprint(self.__dict__)
 
-    # def reload(self):
-    #     """ A beaty method to reload the instance """
-    #     os.system('cls')
-    #     importlib.reload(mongoConnection)
-    #     for name, method in mongoConnection.MongoConnection.__dict__.items():
-    #         if callable(method):
-    #             setattr(self, name, method.__get__(self, self.__class__))
-    #     print(f"{AMARILLO}MongoConnection Reloaded!{GRIS}")
+    def connect(self):
+        try:
+            # self.client = MongoClient(self.uri)
+            self.db = MongoClient(self.host)
+            # self.db = self.client[self.db_name]
+            print(f"{VERDE}Conexión exitosa a MongoDB{GRIS}")
+        except Exception as e:
+            print(f"Error al conectar a MongoDB: {e}")
 
     def reload(self):
         """ A beauty method to reload the instance """
         os.system('cls')
         importlib.reload(mongo)
 
+        methods_names = [name for name, value in self.__dict__.items() if callable(value)] #list(self.__dict__.keys())
         # Limpiar métodos antiguos
-        existing_methods = list(self.__dict__.keys())
+        print(methods_names)
         # print(f"{BLANCO}Methods of current instance: {existing_methods}{GRIS}")
         # print(f"{AZUL}Methods of updated version: {list(mongoConnection.MongoConnection.__dict__.keys())}{GRIS}")
-        for method_name in existing_methods:
+        for method_name in methods_names:
             if method_name not in mongo.MongoConnection.__dict__.keys():
                 print(f"{ROJO}Deleting method: {method_name}(){GRIS}")
                 delattr(self, method_name)
 
         # Actualizar métodos de la clase
         for name, method in mongo.MongoConnection.__dict__.items():
+            # print(name)
             if callable(method):
-                if name not in existing_methods:
+                if name not in methods_names:
                     print(f"{VERDE}Adding method: {name}(){GRIS}")
-                setattr(self, name, method.__get__(self, self.__class__))
+                    setattr(self, name, method.__get__(self, self.__class__))
         print(f"{AMARILLO}MongoConnection Reloaded!{GRIS}")
-
-
-    def hola7(self):
-        print(A)
